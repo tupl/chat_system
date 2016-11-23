6989c2c45f5069d3db76f689546761f43dd19626
Tu Le 261 <thelastdarkman@gmail.com>
2016-11-23 00:21:47 -0800

    Message status

--- src/client/message.js
+++ src/client/message.js
@@ -1,8 +1,17 @@
 import { Record, Map, List } from 'Immutable';
 
+/*
+
+  Enum:
+    sending: If trying to sent to server
+    sent: If already sent
+    received: If receved from server
+*/
+
 class Message extends Record({
   id: -1,
-  content: ""
+  content: "",
+  status: 'Sending'
 }) {
 
 }
