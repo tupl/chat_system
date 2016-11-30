10d3b032965d356cd5595da4a82f4b4300d2fd7a
Tu Le <tupl@uci.edu>
2016-11-30 00:54:46 -0800

    Added first test for messageAction.js

--- src/client/messageapp/test/testMessageAction.js
+++ src/client/messageapp/test/testMessageAction.js
@@ -1,10 +1,30 @@
-let expect = require("chai").expect;
 import { create_MSG_SUBMIT, ACTION_MSG_SUBMIT } from '../messageAction.js';
+import { expect } from 'chai';
 
 describe("testMessageAction module", function() {
 
+  it("Expect ACTION_MSG_SUBMIT has correct value",
+    function() {
+
+      expect(ACTION_MSG_SUBMIT)
+        .to
+        .equal('ACTION_MSG_SUBMIT')
+
+  });
+
   it("Expect create_MSG_SUBMIT follows correct format",
     function() {
+
+      let mess = "This is a test case";
+      let action = create_MSG_SUBMIT(mess);
+
+      expect(action.type)
+        .to
+        .equal('ACTION_MSG_SUBMIT');
+
+      expect(action.content)
+        .to
+        .equal(mess)
 
   });
 
